90553466491e191b9c1da02272f865f8084fb77d
rndomhack <rndom.hack@gmail.com>
2016-09-20 01:01:53 +0900

    Fix display

--- test/test.js
+++ test/test.js
@@ -8,6 +8,7 @@ const TsB25Decoder = require("../index").TsB25Decoder;
 const startTime = Date.now();
 const size = process.argv[2] === "-" ? 0 : fs.statSync(process.argv[2]).size;
 let bytesRead = 0;
+let count = 0;
 
 const readableStream = process.argv[2] === "-" ? process.stdin : fs.createReadStream(process.argv[2]);
 const writableStream = process.argv[3] === "-" ? process.stdout : fs.createWriteStream(process.argv[3]);
@@ -15,8 +16,11 @@ const transformStream = new stream.Transform({
     transform: function (chunk, encoding, done) {
         bytesRead += chunk.length;
 
-        process.stderr.write("\r\u001b[K");
-        process.stderr.write(`Decode - ${bytesRead} of ${size} [${Math.floor(bytesRead / size * 100)}%]`);
+        if (++count === 100) {
+            process.stderr.write("\r\u001b[K");
+            process.stderr.write(`Decode - ${bytesRead} of ${size} [${Math.floor(bytesRead / size * 100)}%]`);
+            count = 0;
+        }
 
         this.push(chunk);
         done();
